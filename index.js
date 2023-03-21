02fb35539fc0cf4a8bb71b832f3dce5964ca969c
= <hellohorleng007@gamail.com>
2023-03-21 16:43:48 +0700

    file-convert

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
-
+const fs = require('fs');
+const multer = require('multer');
 app.use(express.json());
 app.listen(5000,()=>{
     console.log('http://localhost:5000');
 })
-
-app.get('/',(req,res)=>{
-    const {id,username,email} = req.body;
-    res.json({id,username,email});
+const upl = multer({storage:new multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'./');
+    },
+    filename:(req,file,cb)=>{
+        cb(null,'.jpg');
+    }
+})})    
+app.post('/',upl.single('image'),(req,res)=>{
+    const base = fs.readFileSync(req.file.path,{encoding:'base64'});
+    fs.writeFile('image.png',base,{encoding:'base64'},(err,result)=>{
+        console.log('file created successsfully');
+    })
+    res.json({base:"data:image/gif;base64,"+base});
 })
